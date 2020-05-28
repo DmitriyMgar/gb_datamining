33f7510c48246266754006a3ef07415767c564d5
Dmitriy Mgar <mgar@phystech.edu>
2020-05-28 22:47:06 +0300

    scrapy runner

---
+++ runner.py
@@ -0,0 +1,11 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+from gb_parse import settings
+from gb_parse.spiders.gb_blog import GbBlogSpider
+
+if __name__ == '__main__':
+    crawl_settings = Settings()
+    crawl_settings.setmodule(settings)
+    crawl_proc = CrawlerProcess(settings=crawl_settings)
+    crawl_proc.crawl(GbBlogSpider)
+    crawl_proc.start()
