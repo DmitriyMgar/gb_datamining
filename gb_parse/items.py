36d76a9335d8ebea2fc42467510903a611ae6daf
Dmitriy Mgar <mgar@phystech.edu>
2020-06-13 13:47:22 +0300

    Lesson 5: ItemLoader, items preprocessing, Image pipeline

--- gb_parse/items.py
+++ gb_parse/items.py
@@ -6,9 +6,26 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import MapCompose, TakeFirst
+
+
+def gb_text_to_str(item):
+    return ''.join(item)
 
 
 class GbParseItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    _id = scrapy.Field()
+    url = scrapy.Field(output_processor=TakeFirst())
+    title = scrapy.Field(output_processor=TakeFirst())
+    pub_date = scrapy.Field(output_processor=TakeFirst())
+    text_content = scrapy.Field(input_processor=gb_text_to_str, output_processor=TakeFirst())
+    author_url = scrapy.Field(output_processor=TakeFirst())
+    author_name = scrapy.Field(output_processor=TakeFirst())
+    images = scrapy.Field()
+
+
+class AvitoItem(scrapy.Item):
+    _id = scrapy.Field()
+    url = scrapy.Field()
+    title = scrapy.Field()
+    images = scrapy.Field()
--- gb_parse/pipelines.py
+++ gb_parse/pipelines.py
@@ -4,8 +4,32 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy import Request
+from scrapy.pipelines.images import ImagesPipeline
+from scrapy_config import CLIENT_DB
 
 
 class GbParsePipeline:
     def process_item(self, item, spider):
+        collection = CLIENT_DB[spider.name]
+        collection.insert(item)
+        return item
+
+
+class ImgPipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        for url in item.get('images', []):
+            try:
+                yield Request(url)
+            except Exception as e:
+                print(e)
+
+    def item_completed(self, results, item, info):
+        item['images'] = [itm[1] for itm in results]
+        return item
+
+
+class GbParse2Pipeline:
+    def process_item(self, item, spider):
+        print(1)
         return item
